9325c7c93e46190ac2bb5d69f8baf799d6b0807b
Sylvia Joseph <sylviarjoseph@gmail.com>
2024-12-13 16:20:01 +0100

    Update JS Basics - Social Media Post

--- js-basics-and-variables_social-media-post/index.js
+++ js-basics-and-variables_social-media-post/index.js
@@ -11,6 +11,13 @@ console.clear();
 
 // --v-- write your code here --v--
 
+const titlePost = "My Journey";
+const textContentOfPost =
+  "This post describes my trips to different destinations in the World.";
+let numberOfLikes = 300;
+const userCreatedPost = "seeingthe world";
+const isReported = true;
+
 // --^-- write your code here --^--
 
 /*
@@ -18,5 +25,14 @@ console.clear();
 */
 
 // --v-- write your code here --v--
+
+console.log("a title for the post: ", titlePost);
+console.log("text content for the post: ", textContentOfPost);
+console.log("the number of likes the post has received: ", numberOfLikes);
+console.log("the user who created the post: ", userCreatedPost);
+console.log("post has been reported: ", isReported);
+
+numberOfLikes += 1;
+console.log("Updated Count: ", numberOfLikes);
 
 // --^-- write your code here --^--
