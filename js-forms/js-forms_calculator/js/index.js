093f714ad634f003e1fa4f61751bb73a0cc5405d
Sylvia Joseph <sylviarjoseph@gmail.com>
2024-12-20 10:56:09 +0100

    Update JS Forms - Calculator

--- js-forms/js-forms_calculator/js/index.js
+++ js-forms/js-forms_calculator/js/index.js
@@ -26,6 +26,39 @@ form.addEventListener("submit", (event) => {
 
   // --v-- write your code here --v--
 
+  console.log("Event is:", event);
+
+  // We grab all the information of the form.
+  const formData = new FormData(event.target);
+  console.log("Form Data is:", formData);
+
+  // We convert the formData that we dont know how to work with, to an object that we know how to work with.
+  const data = Object.fromEntries(formData);
+  console.log("Data is:", data);
+
+  //All my data object values are string so to work with the number I need to convert them
+
+  const numberA = parseInt(data.numberA);
+  const numberB = parseInt(data["numberB"]);
+
+  console.log("numberA", numberA);
+
+  // Depending on the operator selected, what function we need to call
+  switch (data.operator) {
+    case "addition":
+      result = add(numberA, numberB);
+      break;
+    case "subtarction":
+      result = subtract(numberA, numberB);
+      break;
+    case "multiplication":
+      result = multiply(numberA, numberB);
+      break;
+    case "division":
+      result = divide(numberA, numberB);
+      break;
+  }
+
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
