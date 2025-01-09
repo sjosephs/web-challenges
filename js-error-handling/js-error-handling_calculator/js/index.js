9cacd9779927d6c74a6e405b9b915d1d3a2384ea
Sylvia Joseph <sylviarjoseph@gmail.com>
2025-01-09 12:16:24 +0100

    Update JS Error Handling - Calculator

--- js-error-handling/js-error-handling_calculator/js/index.js
+++ js-error-handling/js-error-handling_calculator/js/index.js
@@ -2,6 +2,7 @@ console.clear();
 
 const form = document.querySelector("form");
 const output = form.querySelector("output");
+const errorElement = document.querySelector(".error");
 
 const operations = {
   add: (a, b) => a + b,
@@ -17,8 +18,16 @@ const operations = {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  errorElement.textContent = "";
   const firstNumber = Number(event.target.firstNumber.value);
   const secondNumber = Number(event.target.secondNumber.value);
   const operation = event.target.operation.value;
-  output.innerText = operations[operation](firstNumber, secondNumber);
+  try {
+    output.innerText = operations[operation](firstNumber, secondNumber);
+  } catch (error) {
+    console.log(error.message);
+    console.error(error.message);
+    console.warn(error);
+    errorElement.textContent = error.message;
+  }
 });
