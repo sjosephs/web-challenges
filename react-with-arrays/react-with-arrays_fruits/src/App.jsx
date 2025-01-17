54ce61059d94dce8b245d3162c22e0ed0453a371
Sylvia Joseph <sylviarjoseph@gmail.com>
2025-01-17 15:42:37 +0100

    Update React with Arrays - Fruits

--- react-with-arrays/react-with-arrays_fruits/src/App.jsx
+++ react-with-arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,11 +1,19 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    { id: 1, name: "🍌 Banana", color: "yellow" },
+    { id: 2, name: "🍏 Apple", color: "green" },
+    { id: 3, name: "🍎 Red Apple", color: "red" },
+    { id: 4, name: "🍇 Grape", color: "purple" },
+    { id: 5, name: "🍓 Strawberry", color: "red" },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruit.id} name={fruit.name} color={fruit.color} /> // dynamically create a Card component for each fruit in the array.key prop to ensure unique keys for each Card.
+      ))}
     </div>
   );
 }
