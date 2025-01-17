d7775bb9846ea8355467a37cd39c4963be94e538
Sylvia Joseph <sylviarjoseph@gmail.com>
2025-01-17 13:36:39 +0100

    Update React State 1 - Counter

--- react-state-1/react-state-1_counter/src/App.jsx
+++ react-state-1/react-state-1_counter/src/App.jsx
@@ -1,26 +1,29 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let count = 0;
+  //let count = 0;
+  const [count, setCount] = useState(0); // Declare state variable for the count
+  console.log("count before", count);
+
+  const handleIncrementCount = () => {
+    //setCount(count + 1);
+    setCount((count) => count + 1);
+    console.log("count after", count);
+  };
+
+  function handleDecrementCount() {
+    setCount(count - 1);
+  }
 
   return (
     <div className="container">
       <h1>{count}</h1>
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={handleDecrementCount}>
           -
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={handleIncrementCount}>
           +
         </button>
       </div>
