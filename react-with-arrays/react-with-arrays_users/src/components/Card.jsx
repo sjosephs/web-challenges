239d50e9b7b613d3a463337b99cae015779814e8
Sylvia Joseph <sylviarjoseph@gmail.com>
2025-01-20 11:13:39 +0100

    Pending React with Arrays - Users

--- react-with-arrays/react-with-arrays_users/src/components/Card.jsx
+++ react-with-arrays/react-with-arrays_users/src/components/Card.jsx
@@ -1,12 +1,16 @@
 import "./Card.css";
 import Tag from "./Tag";
 import Button from "./Button";
+import { USERS } from "../db";
+
 function Card({ user }) {
   return (
     <article className="card">
       <h2>{user.name}</h2>
       <ul className="card__taglist">
-        <Tag tag={user.roles[0]} />
+        {user.roles.map((role) => (
+          <Tag key={role} tag={role} />
+        ))}
       </ul>
       <p>{user.about}</p>
       <Button>edit</Button>
