493306d7462835b6660b0dcd74d178e878d0fc58
Sylvia Joseph <sylviarjoseph@gmail.com>
2025-01-21 12:26:31 +0100

    Update React Effects and Fetch - Pokemon-API

--- react-effects-and-fetch/react-effects-and-fetch_pokemon-api/src/components/PokemonList.jsx
+++ react-effects-and-fetch/react-effects-and-fetch_pokemon-api/src/components/PokemonList.jsx
@@ -1,23 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
-
+  //useEffect(() => {
   async function loadPokemon() {
     try {
       const response = await fetch("https://pokeapi.co/api/v2/pokemon");
       const data = await response.json();
+
       setPokemon(data.results);
     } catch (error) {
       console.log(error);
     }
   }
+  //loadPokemon();
+  //}, []);
+
+  // Fetch Pokémon when the component mounts
+  useEffect(() => {
+    loadPokemon();
+  }, []); // Dependency array ensures this runs only once
 
   return (
     <main>
-      <button type="button" onClick={loadPokemon}>
-        Load Pokémon
-      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
