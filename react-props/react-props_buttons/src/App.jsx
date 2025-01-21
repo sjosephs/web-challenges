3303b7ab3574c017828dadd5a7b7f3fb09bd76fe
Sylvia Joseph <sylviarjoseph@gmail.com>
2025-01-21 22:57:10 +0100

    Update React Props - Buttons, Buttons, Buttons

--- react-props/react-props_buttons/src/App.jsx
+++ react-props/react-props_buttons/src/App.jsx
@@ -1,36 +1,34 @@
 export default function App() {
+  function handleClick(text) {
+    console.log(`${text} clicked me`);
+  }
   return (
     <>
       <Button
         color="blue"
         disabled={false}
         text="Submit"
-        onClick={() => alert("You clicked me!")}
+        onClick={() => handleClick("Submit")}
       />
 
       <Button
         color="red"
         disabled={false}
         text="Dont Submit"
-        onClick={() => alert("You clicked me!")}
+        onClick={() => handleClick("Dont Submit")}
       />
     </>
   );
 }
 
-function Button({
-  color,
-  disabled,
-  text,
-  onClick = () => console.log(`${text} clicked me`),
-}) {
+function Button({ color, disabled, text, onClick }) {
   return (
     <button
       style={{ color, height: "100px", width: "100px" }}
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
-      {disabled}
     </button>
   );
 }
