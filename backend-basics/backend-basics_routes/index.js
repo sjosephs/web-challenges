0fabb4284f5f228f97df4c13a2795eb38d2db635
Sylvia Joseph <sylviarjoseph@gmail.com>
2025-02-06 11:48:43 +0100

    Update Backend Basics - Routes

--- backend-basics/backend-basics_routes/index.js
+++ backend-basics/backend-basics_routes/index.js
@@ -1,3 +1,5 @@
-// import { server } from "./server.js";
+import { server } from "./server.js";
 
-// server.listen(…
+server.listen(8000, () => {
+  console.log("Server up and running on http://localhost:8000");
+});
