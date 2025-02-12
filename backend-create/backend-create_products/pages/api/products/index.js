eb0d47035be7ee79c504b1f73342b57cc0e10ca3
Sylvia Joseph <sylviarjoseph@gmail.com>
2025-02-12 09:40:07 +0100

    Update Backend Create - Products (Fish Shop)

--- backend-create/backend-create_products/pages/api/products/index.js
+++ backend-create/backend-create_products/pages/api/products/index.js
@@ -2,7 +2,18 @@ import dbConnect from "@/db/connect";
 import Product from "@/db/models/Product";
 
 export default async function handler(request, response) {
-  await dbConnect();
+  await dbConnect(); // Ensure database connection
+
+  if (request.method === "POST") {
+    try {
+      const productData = request.body; // Get data from request body
+      console.log("productData", productData);
+      await Product.create(productData); // Save to MongoDB
+      return response.status(201).json({ status: "Product created." });
+    } catch (error) {
+      return response.status(400).json({ error: "Error creating product" });
+    }
+  }
 
   if (request.method === "GET") {
     const products = await Product.find();
